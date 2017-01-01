77280dadba171500a303630f12f83a45bff0f832
IS96355 <IS96355@T1LPRVT968>
2017-01-01 22:05:09 +0300

    Add project files.

---
+++ JenkinsApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "onurunyeri";
+
+            int a = test.Length;
+
+            deneme
+        }
+    }
+}
